faaf7dd1a7dc896c31b52d81285cd798079b756e
Stephen Thirlwall <sdt@dr.com>
2021-04-04 16:59:45 +1000

    Update hello world message

--- hello-world/hello-world.s
+++ hello-world/hello-world.s
@@ -13,29 +13,12 @@ reset:
     hd44780_write_register $0e ; display on, cursor on, blink off
 
     hd44780_write_register $80 ; dram address = 0 => 0,0
-    hd44780_write_data ' '
-    hd44780_write_data 'H'
-    hd44780_write_data 'e'
-    hd44780_write_data 'l'
-    hd44780_write_data 'l'
-    hd44780_write_data 'o'
-    hd44780_write_data ','
-    hd44780_write_data ' '
-    hd44780_write_data 'w'
-    hd44780_write_data 'o'
-    hd44780_write_data 'r'
-    hd44780_write_data 'l'
-    hd44780_write_data 'd'
-    hd44780_write_data '!'
-
-    hd44780_write_register $C0 ; dram address = $40 => 0,1
     hd44780_write_data '6'
     hd44780_write_data '5'
     hd44780_write_data '0'
     hd44780_write_data '2'
     hd44780_write_data ' '
-    hd44780_write_data '@'
-    hd44780_write_data 'B'
+    hd44780_write_data 'b'
     hd44780_write_data 'r'
     hd44780_write_data 'e'
     hd44780_write_data 'a'
@@ -45,6 +28,25 @@ reset:
     hd44780_write_data 'a'
     hd44780_write_data 'r'
     hd44780_write_data 'd'
+    hd44780_write_data '!'
+
+    hd44780_write_register $C0 ; dram address = $40 => 0,1
+    hd44780_write_data 'E'
+    hd44780_write_data 'i'
+    hd44780_write_data 'g'
+    hd44780_write_data 'h'
+    hd44780_write_data 't'
+    hd44780_write_data ' '
+    hd44780_write_data 'b'
+    hd44780_write_data 'i'
+    hd44780_write_data 't'
+    hd44780_write_data ' '
+    hd44780_write_data 'a'
+    hd44780_write_data 'c'
+    hd44780_write_data 't'
+    hd44780_write_data 'i'
+    hd44780_write_data 'o'
+    hd44780_write_data 'n'
 
 loop:
     jmp loop
