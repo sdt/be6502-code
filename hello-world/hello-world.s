86445a5ee9eddab0095363eedab64166745ac36f
Stephen Thirlwall <sdt@dr.com>
2021-04-04 11:31:40 +1000

    Hello world for a ramless system
    All macros

---
+++ hello-world/hello-world.s
@@ -0,0 +1,54 @@
+    .include "6552.inc"
+    .include "hd44780.inc"
+
+    .org $8000
+
+; Code
+reset:
+    hd44780_init
+
+    hd44780_write_register $38 ; 8-bit interface, 2-line display, 5x8 fnot
+    hd44780_write_register $01 ; clear
+    hd44780_write_register $02 ; home
+    hd44780_write_register $0e ; display on, cursor on, blink off
+
+    hd44780_write_register $80 ; dram address = 0 => 0,0
+    hd44780_write_data ' '
+    hd44780_write_data 'H'
+    hd44780_write_data 'e'
+    hd44780_write_data 'l'
+    hd44780_write_data 'l'
+    hd44780_write_data 'o'
+    hd44780_write_data ','
+    hd44780_write_data ' '
+    hd44780_write_data 'w'
+    hd44780_write_data 'o'
+    hd44780_write_data 'r'
+    hd44780_write_data 'l'
+    hd44780_write_data 'd'
+    hd44780_write_data '!'
+
+    hd44780_write_register $C0 ; dram address = $40 => 0,1
+    hd44780_write_data '6'
+    hd44780_write_data '5'
+    hd44780_write_data '0'
+    hd44780_write_data '2'
+    hd44780_write_data ' '
+    hd44780_write_data '@'
+    hd44780_write_data 'B'
+    hd44780_write_data 'r'
+    hd44780_write_data 'e'
+    hd44780_write_data 'a'
+    hd44780_write_data 'd'
+    hd44780_write_data 'b'
+    hd44780_write_data 'o'
+    hd44780_write_data 'a'
+    hd44780_write_data 'r'
+    hd44780_write_data 'd'
+
+loop:
+    jmp loop
+
+    ; Interrupt vector table.
+irq = $efbe
+    .include "vectors.inc"
